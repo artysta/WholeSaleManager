7eb6c99ff2a394a3ba6d62eb92e666a7f02b8585
Adrian Kurek <adrian.kurek93@gmail.com>
2020-12-10 17:43:14 +0100

    Add unit of work

--- WholeSaleManager.Web/Startup.cs
+++ WholeSaleManager.Web/Startup.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using WholeSaleManager.DataAccess.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WholeSaleManager.DataAccess.Data;
+using WholeSaleManager.DataAccess.Repository;
+using WholeSaleManager.DataAccess.Repository.IRepository;
 
 namespace WholeSaleManager.Web
 {
@@ -32,6 +28,7 @@ namespace WholeSaleManager.Web
 					Configuration.GetConnectionString("DefaultConnection")));
 			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
 				.AddEntityFrameworkStores<ApplicationDbContext>();
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
 			services.AddControllersWithViews();
 			services.AddRazorPages();
 		}
