86c4d7cd1730fc9969916add0b884c9e10bccbb2
Adrian Kurek <adrian.kurek93@gmail.com>
2020-12-10 17:32:35 +0100

    Add category repository

---
+++ WholeSaleManager.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using WholeSaleManager.DataAccess.Data;
+using WholeSaleManager.DataAccess.Repository.IRepository;
+using WholeSaleManager.Models;
+using System.Linq;
+
+namespace WholeSaleManager.DataAccess.Repository
+{
+	public class CategoryRepository : Repository<Category>, ICategoryRepository
+	{
+		private readonly ApplicationDbContext _db;
+
+		public CategoryRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+
+		public void Update(Category category)
+		{
+			var obj = _db.Categories.FirstOrDefault(c => c.Id == category.Id);
+
+			if (obj != null)
+			{
+				obj.Name = category.Name;
+				_db.SaveChanges();
+			}
+		}
+	}
+}
---
+++ WholeSaleManager.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using WholeSaleManager.Models;
+
+namespace WholeSaleManager.DataAccess.Repository.IRepository
+{
+	public interface ICategoryRepository : IRepository<Category>
+	{
+		void Update(Category category);
+	}
+}
