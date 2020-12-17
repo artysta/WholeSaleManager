c986055932a0ab2134e7e80c5cdb749578f91226
Adrian Kurek <adrian.kurek93@gmail.com>
2020-12-17 20:07:15 +0100

    Add manufacturer model

---
+++ WholeSaleManager.Models/Manufacturer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WholeSaleManager.Models
+{
+	public class Manufacturer
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Display(Name = "Manufacturer")]
+		[Required]
+		[MaxLength(55)]
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
