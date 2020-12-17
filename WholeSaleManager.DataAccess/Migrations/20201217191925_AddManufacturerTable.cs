d2e44890fc763ecffb7ed7c1038266f338ca28dc
Adrian Kurek <adrian.kurek93@gmail.com>
2020-12-17 20:28:19 +0100

    Add manufacturer migration

---
+++ WholeSaleManager.DataAccess/Migrations/20201217191925_AddManufacturerTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WholeSaleManager.DataAccess.Migrations
+{
+    public partial class AddManufacturerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Manufacturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 55, nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manufacturers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Manufacturers");
+        }
+    }
+}
