94d223d72f9b231f1a425842bc945b353830f361
Adrian Kurek <adrian.kurek93@gmail.com>
2020-12-18 12:03:59 +0100

    Add views & controller for manufacturer

---
+++ WholeSaleManager.Web/Areas/Admin/Controllers/ManufacturerController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WholeSaleManager.DataAccess.Repository.IRepository;
+using WholeSaleManager.Models;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+    public class ManufacturerController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ManufacturerController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Manufacturer manufacturer = new Manufacturer();
+            if (id == null)
+            {
+                return View(manufacturer);
+            }
+
+            manufacturer = _unitOfWork.Manufacturer.Get(id.GetValueOrDefault());
+            
+            if (manufacturer == null)
+            {
+                return NotFound();
+            }
+            
+            return View(manufacturer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Manufacturer manufacturer)
+        {
+            if (ModelState.IsValid)
+            {
+                if (manufacturer.Id == 0)
+                {
+                    _unitOfWork.Manufacturer.Add(manufacturer);
+
+                }
+                else
+                {
+                    _unitOfWork.Manufacturer.Update(manufacturer);
+                }
+
+                _unitOfWork.Save();
+                
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(manufacturer);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Manufacturer.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Manufacturer.Get(id);
+
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting the manufacturer!" });
+            }
+
+            _unitOfWork.Manufacturer.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "You have successfully removed the manufacturer!" });
+        }
+
+        #endregion
+    }
+}
---
+++ WholeSaleManager.Web/wwwroot/js/manufacturer.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Manufacturer/GetAll"
+        },
+        "columns": [
+            {
+                "data": "name",
+                "wdith": "60%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/Manufacturer/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    Edit
+                                </a>
+                                <a onclick=Delete("/Admin/Manufacturer/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    Delete
+                                </a>
+                            </div>
+                            `;
+                },
+                "width": "40%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Do you really want to delete this manufacturer?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((d) => {
+        if (d) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
