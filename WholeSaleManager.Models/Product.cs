764a721f6fda419199ecee8fb8f4f5ac56b4d0e8
Adrian Kurek <adrian.kurek93@gmail.com>
2020-12-20 19:55:12 +0100

    Add product model

---
+++ WholeSaleManager.Models/Product.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WholeSaleManager.Models
+{
+	public class Product
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		[Required]
+		[Range(1, 10000)]
+		public double ListPrice { get; set; }
+
+		// Price which customer will pay, if he orders less than 50 products.
+		[Required]
+		[Range(1, 10000)]
+		public double Price { get; set; }
+
+		// Price which customer will pay, if he orders 50 - 99 products.
+		[Required]
+		[Range(1, 10000)]
+		public double Price50 { get; set; }
+
+		// Price which customer will pay, if he orders more than 100 products.
+		[Required]
+		[Range(1, 10000)]
+		public double Price100 { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		[Required]
+		public string CategoryId { get; set; }
+
+		[ForeignKey("CategoryId")]
+		public Category Category { get; set; }
+
+		[Required]
+		public string ManufacturerId { get; set; }
+
+		[ForeignKey("ManufacturerId")]
+		public Manufacturer Manufacturer { get; set; }
+	}
+}
