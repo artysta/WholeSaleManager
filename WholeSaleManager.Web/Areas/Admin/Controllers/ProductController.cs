1215bca09c0031d38a9250f627bc527b38a7eca1
Adrian Kurek <adrian.kurek93@gmail.com>
2020-12-21 18:50:39 +0100

    Add product controller

---
+++ WholeSaleManager.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using WholeSaleManager.DataAccess.Repository.IRepository;
+using WholeSaleManager.Models;
+using WholeSaleManager.Models.ViewModels;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            ProductViewModel productViewModel = new ProductViewModel()
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                ManufacturerList = _unitOfWork.Manufacturer.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+
+            if (id == null)
+            {
+                return View(productViewModel);
+            }
+
+            productViewModel.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            
+            if (productViewModel.Product == null)
+            {
+                return NotFound();
+            }
+            
+            return View(productViewModel.Product);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll(includeProperties: "Category, Manufacturer");
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Product.Get(id);
+
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting the product!" });
+            }
+
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "You have successfully removed the product!" });
+        }
+
+        #endregion
+    }
+}
