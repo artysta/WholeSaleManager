dc7d28c4ea61e33ea23386133cdf58d00114230c
Adrian Kurek <adrian.kurek93@gmail.com>
2020-12-22 18:51:05 +0100

    Add index & upsert views for product

---
+++ WholeSaleManager.Web/wwwroot/js/product.js
@@ -0,0 +1,73 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Product/GetAll"
+        },
+        "columns": [
+            {
+                "data": "name",
+                "width": "15%"
+            },
+            {
+                "data": "description",
+                "width": "15%"
+            },
+            {
+                "data": "price",
+                "width": "15%"
+            },
+            {
+                "data": "category.Name",
+                "width": "15%"
+            },
+            {
+                "data": "manufacturer.Name",
+                "width": "15%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/Product/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    Edit
+                                </a>
+                                <a onclick=Delete("/Admin/Product/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    Delete
+                                </a>
+                            </div>
+                            `;
+                },
+                "width": "40%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Do you really want to delete this product?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((d) => {
+        if (d) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
