0ff2295d3d9515e152ef06d22ca29fa76f8f8ad0
Adrian Kurek <adrian.kurek93@gmail.com>
2020-12-22 21:50:39 +0100

    Add action for post method

--- WholeSaleManager.Web/Areas/Admin/Controllers/ProductController.cs
+++ WholeSaleManager.Web/Areas/Admin/Controllers/ProductController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using WholeSaleManager.DataAccess.Repository.IRepository;
 using WholeSaleManager.Models;
 using WholeSaleManager.Models.ViewModels;
+using System;
+using System.IO;
 
 namespace BulkyBook.Areas.Admin.Controllers
 {
@@ -56,12 +58,88 @@ namespace BulkyBook.Areas.Admin.Controllers
             return View(productViewModel.Product);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _webHostEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+
+                if (files.Count > 0)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\products");
+                    var extenstion = Path.GetExtension(files[0].FileName);
+
+                    if (productViewModel.Product.ImageUrl != null)
+                    {
+                        var imagePath = Path.Combine(webRootPath, productViewModel.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+                    }
+
+                    using (var filesStreams = new FileStream(Path.Combine(uploads, fileName + extenstion), FileMode.Create))
+                    {
+                        files[0].CopyTo(filesStreams);
+                    }
+
+                    productViewModel.Product.ImageUrl = @"\images\products\" + fileName + extenstion;
+                }
+                else
+                {
+                    if (productViewModel.Product.Id != 0)
+                    {
+                        Product objFromDb = _unitOfWork.Product.Get(productViewModel.Product.Id);
+                        productViewModel.Product.ImageUrl = objFromDb.ImageUrl;
+                    }
+                }
+
+                if (productViewModel.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productViewModel.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productViewModel.Product);
+                }
+
+                _unitOfWork.Save();
+                
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                productViewModel.CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                });
+
+                productViewModel.ManufacturerList = _unitOfWork.Manufacturer.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                });
+
+                if (productViewModel.Product.Id != 0)
+                {
+                    productViewModel.Product = _unitOfWork.Product.Get(productViewModel.Product.Id);
+                }
+            }
+
+            return View(productViewModel);
+        }
+
         #region API CALLS
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Category.GetAll(includeProperties: "Category, Manufacturer");
+            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category, Manufacturer");
             return Json(new { data = allObj });
         }
 
