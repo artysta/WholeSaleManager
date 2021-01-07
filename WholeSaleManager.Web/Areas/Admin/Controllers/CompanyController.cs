3a776fb86a8254e66639fb1b971377d3f09484f2
Adrian Kurek <adrian.kurek93@gmail.com>
2021-01-07 17:40:00 +0100

    Add company controller

---
+++ WholeSaleManager.Web/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WholeSaleManager.DataAccess.Repository.IRepository;
+using WholeSaleManager.Models;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Company company = new Company();
+            if (id == null)
+            {
+                return View(company);
+            }
+
+            company = _unitOfWork.Company.Get(id.GetValueOrDefault());
+            
+            if (company == null)
+            {
+                return NotFound();
+            }
+            
+            return View(company);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                if (company.Id == 0)
+                {
+                    _unitOfWork.Company.Add(company);
+
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(company);
+                }
+
+                _unitOfWork.Save();
+                
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(company);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Company.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Company.Get(id);
+
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting the company!" });
+            }
+
+            _unitOfWork.Company.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "You have successfully removed the company!" });
+        }
+
+        #endregion
+    }
+}
