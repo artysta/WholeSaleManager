45d2919d19417f5e923e95c00682b37bb7957afe
Adrian Kurek <adrian.kurek93@gmail.com>
2021-01-07 17:40:38 +0100

    Add company ajax

---
+++ WholeSaleManager.Web/wwwroot/js/company.js
@@ -0,0 +1,84 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Company/GetAll"
+        },
+        "columns": [
+            {
+                "data": "name",
+                "wdith": "15%"
+            },
+            {
+                "data": "streetAddress",
+                "wdith": "15%"
+            },
+            {
+                "data": "city",
+                "wdith": "10%"
+            },
+            {
+                "data": "state",
+                "wdith": "10%"
+            },
+            {
+                "data": "phoneNumber",
+                "wdith": "15%"
+            },
+            {
+                "data": "isAuthorizedCompany",
+                "render": function (data) {
+                    if (data) {
+                        return `<input type="checkbox" disabled checked />`
+                    } else {
+                        return `<input type="checkbox" disabled />`
+                    }
+                },
+                "wdith": "10%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/Company/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    Edit
+                                </a>
+                                <a onclick=Delete("/Admin/Company/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    Delete
+                                </a>
+                            </div>
+                            `;
+                },
+                "width": "40%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Do you really want to delete this category?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((d) => {
+        if (d) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
