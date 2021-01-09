93a0f5b57b47bfb1ddd9b8f7b562d8c2fd955096
nat13alia <natalia13krawczyk@gmail.com>
2021-01-10 00:56:39 +0100

    updated the solution to .NET Core 5.0, scaffoldded Identity Razor Page and added fields to registration Page

---
+++ WholeSaleManager.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WholeSaleManager.DataAccess.Data;
+
+[assembly: HostingStartup(typeof(WholeSaleManager.Web.Areas.Identity.IdentityHostingStartup))]
+namespace WholeSaleManager.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
