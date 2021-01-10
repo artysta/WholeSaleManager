184c097675feea601ef0dc0e6adcb6360684f53e
nat13alia <natalia13krawczyk@gmail.com>
2021-01-10 20:07:31 +0100

    Added Roles to StaticDetails.cs

--- WholeSaleManager.Utility/StaticDetails.cs
+++ WholeSaleManager.Utility/StaticDetails.cs
@@ -2,5 +2,9 @@
 {
 	public static class StaticDetails
 	{
+		public const string Role_User_Indi = "Individual Customer";
+		public const string Role_User_Comp = "Company Customer";
+		public const string Role_Admin = "Admin";
+		public const string Role_Employee = "Employee";
 	}
 }
