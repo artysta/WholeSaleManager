cd34d09eeaac51ec312a4c37d33513f86821d30f
nat13alia <natalia13krawczyk@gmail.com>
2021-01-26 22:42:06 +0100

    Added method for calculation of price based on the amount of products in the cart

--- WholeSaleManager.Utility/StaticDetails.cs
+++ WholeSaleManager.Utility/StaticDetails.cs
@@ -6,5 +6,24 @@
 		public const string Role_User_Comp = "Company Customer";
 		public const string Role_Admin = "Admin";
 		public const string Role_Employee = "Employee";
+
+		public static double GetTotalPrice(double quantity, double price, double price50, double price100)
+        {
+			if(quantity < 50)
+            {
+				return price;
+            }
+			else
+            {
+				if(quantity < 100)
+                {
+					return price50;
+                }
+                else
+                {
+					return price100;
+                }
+            }
+        }
 	}
 }
