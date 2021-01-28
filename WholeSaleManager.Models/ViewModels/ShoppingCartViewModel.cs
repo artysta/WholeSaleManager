509826255cacf6f082923a982dc887f0a46121f5
nat13alia <natalia13krawczyk@gmail.com>
2021-01-28 18:39:36 +0100

    Added ShoppingCartViewModel.cs

---
+++ WholeSaleManager.Models/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WholeSaleManager.Models.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public IEnumerable<ShoppingCart> Carts { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
