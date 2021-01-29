78b558ccc40aee23306db0041672aa212bc6b2ab
nat13alia <natalia13krawczyk@gmail.com>
2021-01-30 00:21:56 +0100

    Implemented Increment Action Method

--- WholeSaleManager.Web/Areas/Customer/Controllers/CartController.cs
+++ WholeSaleManager.Web/Areas/Customer/Controllers/CartController.cs
@@ -56,5 +56,17 @@ namespace WholeSaleManager.Web.Areas.Customer.Controllers
 
             return View(SCVM);
         }
+
+        public IActionResult Increment(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(
+                c => c.Id == cartId, includeProperties: "Product");
+
+            cart.Count += 1;
+            cart.Price = StaticDetails.GetTotalPrice(
+                cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
