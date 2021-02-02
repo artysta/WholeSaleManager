974f0d2bbc0ff51040e9dcea5388808ddbd59c4d
nat13alia <natalia13krawczyk@gmail.com>
2021-02-02 21:21:52 +0100

    Added Statuses to StaticDetails

--- WholeSaleManager.Utility/StaticDetails.cs
+++ WholeSaleManager.Utility/StaticDetails.cs
@@ -9,6 +9,20 @@
 
 		public const string sessionShoppingCart = "Shopping cart Session";
 
+		public const string StatusPending = "Pending";
+		public const string StatusApproved = "Approved ";
+		public const string StatusInProcess = "InProcess ";
+		public const string StatusShipped= "Shipped ";
+		public const string StatusCancelled = "Cancelled ";
+		public const string StatusRefunded = "Refunded ";
+
+		public const string PaymentStatusPending = "Pending";
+		public const string PaymentStatusApproved = "Approved ";
+		public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment ";
+		public const string PaymentStatusRejected = "Rejected  ";
+
+
+
 		public static double GetTotalPrice(double quantity, double price, double price50, double price100)
         {
 			if(quantity < 50)
